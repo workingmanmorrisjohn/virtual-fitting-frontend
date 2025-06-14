c093675e51ed1fd49ca726bc832046d224239f4a
workingmanmorrisjohn <morris.montemayor@azcalun.com>
2025-06-14 11:21:28 +0800

    capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.virtual.system',
+  appName: 'Virtual Fitting System',
+  webDir: 'dist'
+};
+
+export default config;
